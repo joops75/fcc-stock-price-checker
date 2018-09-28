4ef890ae55f5fee6e2051a3e1f71c9a32254030a
Simon Cooper <kidney9@hotmail.com>
2018-09-28 14:26:15 +0100

    routes completed

--- routes/api.js
+++ routes/api.js
@@ -8,16 +8,18 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+// var expect = require('chai').expect;
+// var MongoClient = require('mongodb');
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+// const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+
+const StockController = require('../controllers/stockController');
 
 module.exports = function (app) {
 
+  const stockController = new StockController();
+
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
+    .get(stockController.getStockPrice);
     
 };
