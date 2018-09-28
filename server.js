eaf6e13f77cd75604a26eebbf137c31511bff384
Simon Cooper <kidney9@hotmail.com>
2018-09-28 14:25:16 +0100

    helmetjs protection and mongoose connection added

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
+const helmet      = require('helmet');
+const mongoose    = require('mongoose');
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
 
-var app = express();
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
+
+app.use(helmet.contentSecurityPolicy({ directives: { scriptSrc: ["'self"] } }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -38,15 +47,15 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
+app.listen(port, function () {
+  console.log("Listening on port " + port);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
       } catch(e) {
-        var error = e;
+        const error = e;
           console.log('Tests are not valid:');
           console.log(error);
       }
